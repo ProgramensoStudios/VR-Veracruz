b441931fea3ab9d16a9a5c731312183be199ccbc
Kenny <alcanamay@gmail.com>
2025-12-17 12:05:45 -0600

    Shader y empezando Global Volume
    aaaaa pa mergear

---
+++ VRPlaybookGame/Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class VolumeManager : MonoBehaviour
+{
+    public Volume vol;
+
+    private void Start()
+    {
+        
+    }
+}
